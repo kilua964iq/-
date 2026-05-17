986217d9c94d46b1b6cc044ab144db291f5faafb
kilua964iq <gookilua@gmail.com>
2026-05-17 04:43:45 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+print("TOKEN:", os.getenv("BOT_TOKEN", "NOT FOUND"))
+print("API_ID:", os.getenv("API_ID", "NOT FOUND"))
+print("API_HASH:", os.getenv("API_HASH", "NOT FOUND"))
+print("OWNER_ID:", os.getenv("OWNER_ID", "NOT FOUND"))
+print("DATABASE_URL:", os.getenv("DATABASE_URL", "NOT FOUND"))
